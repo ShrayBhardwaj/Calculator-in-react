4f136fef84793279123ca4bd463de56e1d4e3344
Nikandan <it.dextrous@gmail.com>
2021-04-02 09:38:19 +0530

    first commit

---
+++ src/Components/Calculatortitle.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const Calculatortitle = (props) => {
+  return (
+    <div className="calculator-title">
+      { props.value }
+    </div>
+  )
+}
+
+
+export default Calculatortitle;
